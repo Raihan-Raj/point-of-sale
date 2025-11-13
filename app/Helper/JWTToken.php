67c00e3dab28ad636c433b4c50890b53a139604d
Raju <rajdiu8889@gmail.com>
2025-11-13 17:59:16 +0600

    JWT for authentication

---
+++ app/Helper/JWTToken.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Helper;
+
+use Exception;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTToken
+{
+
+    function CreateToken($userEmail): string
+    {
+        $key = env('JWT_KEY');
+        $payload = [
+            'iss' => 'laravel-token', //issuare name
+            'iat' => time(), //current time
+            'exp' => time() + 60 * 60,
+            'userEmail' => $userEmail
+        ];
+        return JWT::encode($payload, $key, 'HS256');
+    }
+
+    function VerifyToken($token)
+    {
+        try {
+            $key = env('JWT_KEY');
+            $decode = JWT::decode($token, new Key($key, 'HS256'));
+            return $decode->userEmail;
+        } catch (Exception $e) {
+            return 'unauthorized';
+        }
+    }
+}
