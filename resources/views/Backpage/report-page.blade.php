c5d2f736fa8f76780f4a15b44b7d6827721f45be
Raju <rajdiu8889@gmail.com>
2026-02-12 00:12:13 +0600

    Report Generation Front End page UI design update

--- resources/views/Backpage/report-page.blade.php
+++ resources/views/Backpage/report-page.blade.php
@@ -1,25 +1,42 @@
-@extends('layout.sidenav-layout')
+@extends('layout.app')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <br>
-                    <br><br>
-                    <h4>Sales Report</h4>
-                    <label class="form-label mt-2">Date From</label>
-                    <input id="FormDate" type="date" class="form-control">
-                    <label class="form-label mt-2">Date To</label>
-                    <input id="ToDate" type="date" class="form-control">
-                    <button onclick="SalesReport()" class="btn mt-3 bg-gradient-primary">Download</button>
+<div class="container-fluid d-flex justify-content-center align-items-center" style="min-height: 80vh;">
+    <div class="col-md-5 col-lg-4">
+        <div class="card shadow-lg border-0 rounded-4">
+            
+            <!-- Card Header -->
+            <div class="card-header bg-gradient-primary text-white text-center rounded-top-4 py-3">
+                <h4 class="mb-0 text-black">
+                    <i class="fas fa-chart-line me-2 text-black"></i> Sales Report
+                </h4>
+            </div>
+
+            <!-- Card Body -->
+            <div class="card-body p-4">
+
+                <div class="mb-3">
+                    <label class="form-label fw-semibold">Date From</label>
+                    <input id="FormDate" type="date" class="form-control form-control-lg rounded-3">
                 </div>
+
+                <div class="mb-4 mt-5">
+                    <label class="form-label fw-semibold">Date To</label>
+                    <input id="ToDate" type="date" class="form-control form-control-lg rounded-3">
+                </div>
+
+                <div class="d-grid mt-5">
+                    <button onclick="SalesReport()" 
+                        class="btn btn-primary btn-lg rounded-3 shadow-sm">
+                        <i class="fas fa-download me-2"></i> Download Report
+                    </button>
+                </div>
+
             </div>
         </div>
     </div>
 </div>
 
-@endsection
+
 <script>
 
     function SalesReport() {
